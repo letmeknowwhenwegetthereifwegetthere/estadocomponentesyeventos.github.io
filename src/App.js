ea290c314f8eec2bfbbfcc5a94fc01863fe596b1
¨Marce¨ <marcelapenalozagonzalez@gmail.com>
2023-05-22 17:01:50 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Formulario from './Components/Formulario';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="container">
+          <Formulario titulo="Practicando" subtitulo="Estado de los componentes y eventos"></Formulario>
+        </div>
     </div>
-  );
-}
+  )
+};
 
-export default App;
+export default App; 
